d4c7abe98cb2d6f1011add21c2bca39e60aa4b47
Fredrik Carlsson <fredrik.j.carlsson@unipower.se>
2025-06-04 11:57:07 +0200

    Updates packages and default font
    Updates the list of explicitly installed packages.
    Removes duplicated, obsolete comments from config.

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default)))
+ '(package-selected-packages '(copilot copilot-chat helm)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
